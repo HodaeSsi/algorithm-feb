b6c1c61bea66c4dcab971d57eecad82d23f5ec1c
seonghyeok cho <65901319+whqtker@users.noreply.github.com>
2026-02-25 11:30:33 +0900

    .

--- 4주차/15일차/조성혁.cpp
+++ 4주차/15일차/조성혁.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <tuple>
-#include <set>
-#include <queue>
 
 using namespace std;
 
 int parent[100001];
+vector<tuple<int,int,int>> v; // x1, x2, num
 
 int Find(int x) {
     int px=parent[x];
@@ -23,6 +23,15 @@ void Union(int x, int y) {
     }
 }
 
+void chk(int cur,int nxt) {
+    auto a=v[cur];
+    auto b=v[nxt];
+
+    if (get<1>(a)>=get<0>(b)) {
+        Union(get<2>(a),get<2>(b));
+    }
+}
+
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
@@ -30,54 +39,24 @@ int main() {
     int n,q;
     cin>>n>>q;
 
-    vector<tuple<int,int,int>> v; // x1, x2, y
-    priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<>> pq; // x좌표, command(1이면 삭제, 0이면 삽입), 통나무 번호
-    v.push_back({0,0,0}); // 보정
-    for (int i=1;i<=n;i++) {
+    for (int i=0;i<n;i++) {
         int x1,x2,y;
         cin>>x1>>x2>>y;
 
-        v.push_back({x1,x2,y});
-        pq.push({x1,0,i});
-        pq.push({x2,1,i});
+        v.push_back({x1,x2,i+1});
     }
+
+    sort(v.begin(),v.end());
 
     for (int i=0;i<=n;i++) {
         parent[i]=i;
     }
 
-    set<pair<int,int>> s; // y, num
-    while (!pq.empty()) {
-        int cmd=get<1>(pq.top());
-        int num=get<2>(pq.top());
-        int y=get<2>(v[num]);
-        pq.pop();
+    int maxIdx=0;
+    for (int i=1;i<n;i++) {
+        chk(maxIdx,i);
 
-        // 삽입 후 나와 인접 통나무들과 Union
-        if (!cmd) {
-            s.insert({y,num});
-            auto it=s.find({y,num});
-
-            if (it!=s.begin()) {
-                auto prv=prev(it);
-                Union(num,prv->second);
-            }
-
-            auto nxt=next(it);
-            if (nxt!=s.end()) {
-                Union(num,nxt->second);
-            }
-        }
-        // 삭제 후 인접 통나무 간 Union
-        else {
-            auto it=s.find({y,num});
-            auto nxt=next(it);
-            if (it!=s.begin()&&nxt!=s.end()) {
-                auto prv=prev(it);
-                Union(prv->second,nxt->second);
-            }
-            s.erase({y,num});
-        }
+        if (get<1>(v[i])>get<1>(v[maxIdx])) maxIdx=i;
     }
 
     for (int i=0;i<q;i++) {
